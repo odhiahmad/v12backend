8a9abdc990b79ea127c5769bdfc63584cd8590c5
odhiahmad <odhiahmad15@gmail.com>
2021-07-17 06:32:04 +0700

    tinggal fungsi modul

---
+++ src/inputs/BSelectValidations.vue
@@ -0,0 +1,63 @@
+<template>
+  <ValidationProvider
+    :vid="vid"
+    :name="$attrs.label"
+    :rules="rules"
+    v-slot="{ errors, valid }"
+  >
+    <b-field
+      v-bind="$attrs"
+      :type="{ 'is-danger': errors[0], 'is-success': valid }"
+      :message="errors"
+    >
+      <v-select :options="options" v-model="innerValue"> </v-select>
+    </b-field>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+
+export default {
+  components: {
+    ValidationProvider,
+  },
+  props: {
+    placeholder: {
+      type: String,
+    },
+    vid: {
+      type: String,
+    },
+    options: {
+      type: [Array],
+    },
+    rules: {
+      type: [Object, String],
+      default: "",
+    },
+    // must be included in props
+    value: {
+      type: null,
+    },
+  },
+  data: () => ({
+    innerValue: "",
+  }),
+  watch: {
+    // Handles internal model changes.
+    innerValue(newVal) {
+      this.$emit("input", newVal);
+    },
+    // Handles external model changes.
+    value(newVal) {
+      this.innerValue = newVal;
+    },
+  },
+  created() {
+    if (this.value) {
+      this.innerValue = this.value;
+    }
+  },
+};
+</script>
